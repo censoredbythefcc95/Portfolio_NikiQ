2a3da86f53a867a4fb2620d4fe89d23ecb195370
Steven <sastudillo6@gmail.com>
2021-03-12 22:18:46 -0500

    Basic home page copied from original site.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavigationBar from "./components/Header";
+import HomePage from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <HomePage />
     </div>
   );
 }
